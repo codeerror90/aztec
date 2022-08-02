de246d05af897c1fb15b96ef1f90ba22ed6e5e1c
codeerror90 <79945630+codeerror90@users.noreply.github.com>
2022-08-02 17:13:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('NekoBot MD', {
+say('Aztec~Bot MD', {
   font: 'tiny',
   align: 'center',
   gradient: ['red', 'magenta']
